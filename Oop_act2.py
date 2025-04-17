612123b4c853439aefa6655ea21c946064e78bf1
Vosty17 <stephwafula70@gmail.com>
2025-04-17 23:50:42 +0300

    Answers to activity 2

--- Oop_act2.py
+++ Oop_act2.py
@@ -1,1 +1,25 @@
 
+class Animal:
+    def __init__(self, name):
+        self.name = name
+        
+    def move(self):
+        pass  # This will be overridden by subclasses
+        
+class Bird(Animal):
+    def move(self):
+        print(f"{self.name} is flying! 🦅")
+        
+class Fish(Animal):
+    def move(self):
+        print(f"{self.name} is swimming! 🐠")
+        
+class Snake(Animal):
+    def move(self):
+        print(f"{self.name} is slithering! 🐍")
+        
+# Example usage
+animals = [Bird("Eagle"), Fish("Nemo"), Snake("Viper")]
+
+for animal in animals:
+    animal.move()  # Each animal moves differently - polymorphism in action!
